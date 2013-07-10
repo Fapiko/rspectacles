d2d0885fa771974e0cce434b428151c093203c49
Lucas Jandrew <fapiko@gmail.com>
2013-07-09 22:43:01 -0700

    Partway through chapter 7

--- codebreaker/lib/codebreaker/game.rb
+++ codebreaker/lib/codebreaker/game.rb
@@ -5,12 +5,21 @@ module Codebreaker
     end
 
     def start(secret_code)
+      @secret_code = secret_code
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
     end
 
     def guess(guess)
+      if guess[0] == @secret_code[0]
+        mark = '+'
+      elsif @secret_code.include?(guess[0])
+        mark = '-'
+      else
+        mark = ''
+      end
 
+      @output.puts mark
     end
   end
 end
